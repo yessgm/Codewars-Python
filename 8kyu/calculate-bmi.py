4425ad9e7fbe6e69113af7640b4044d5036cb30c
Yessica Gaona <yessicagaonam@gmail.com>
2021-12-14 22:44:47 -0600

    codewars practice

---
+++ 8kyu/calculate-bmi.py
@@ -0,0 +1,9 @@
+def bmi(weight, height):
+    bmi = weight / (height**2)
+    if bmi <= 18.5:
+        return "Underweight"
+    if bmi <= 25.0:
+        return "Normal"
+    if bmi <= 30.0:
+        return "Overweight"
+    return "Obese"
