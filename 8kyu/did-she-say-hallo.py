8925f37c54f73822793515423381b59cedfd954b
Yessica Gaona <yessicagaonam@gmail.com>
2021-12-28 23:20:43 -0600

    codewars practice

--- 8kyu/did-she-say-hallo.py
+++ 8kyu/did-she-say-hallo.py
@@ -1,11 +1,2 @@
-def calculate_age(birth, current):
-    if birth == current:
-        return "You were born this very year!"
-    elif birth-current ==1:
-        return "You will be born in 1 year."
-    elif current < birth:
-        return "You will be born in " + str(birth-current) + " years."
-    elif current-birth ==1:
-        return "You are 1 year old."
-    else:
-        return "You are " + str(current-birth) + " years old."
+def validate_hello(greetings):
+    return any(x in greetings.lower() for x in ['hello','ciao','salut','hallo','hola','ahoj','czesc'])
